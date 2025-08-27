905a2b5b2908a629286b0703667be92ca183899e
vcheungg <vivianncheungg@gmail.com>
2025-08-27 12:36:22 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/vcheungg" target="_blank" rel="noreferrer">
-          Vivian Cheung
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/vcheungg/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/vcheungg"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Vivian Cheung
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/vcheungg/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
